e364c9335df1272c616443489b88340faa6b69db
S den Hollander <302045198@student.rocmondriaan.nl>
2023-09-18 12:53:50 +0200

    update

--- iets.js
+++ iets.js
@@ -1,7 +1,17 @@
 
-console.log("poep")
-
+console.log("poep");
 function roll() {
-    document.getElementById("iets!").innerHTML = 'jE HEBT VERLOREN MOGOOL'
+    document.querySelectorclass = ("iets!").innerHTML = 'oei';
 }
 
+
+let mynumber = Math.random();
+// console.log('random getal is ' + myNumber);
+
+function getRandomint (max) {
+ return Math.floor(Math.random() * max);
+}
+
+myNumber = getRandomint(6);
+myNumber = myNumber + 1;
+console.log('random getal is ' + myNumber);
