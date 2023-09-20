48e892ff447f9286c78014f66062a71d480bcd63
S den Hollander <302045198@student.rocmondriaan.nl>
2023-09-20 12:30:51 +0200

    update

---
+++ main.js
@@ -0,0 +1,20 @@
+
+console.log("main loaded");
+
+let myNumber = 1;
+
+
+myNumber = getRandomNumber(6);
+
+myNumber = myNumber + 1;
+console.log('random getal is ' + myNumber);
+document.querySelectorClass = ("something").innerHTML = 'je hebt verloren';
+
+
+function getRandomNumber (max) {
+    return Math.floor(Math.random() * max);
+}
+
+function rollDice() {
+    document.querySelectorClass = ("something").innerHTML = 'je hebt verloren';
+}
