871c976c9e5628b1133d7a65947ccc6a7f90e7a9
S den Hollander <302045198@student.rocmondriaan.nl>
2023-09-27 12:53:33 +0200

    UPDATE

---
+++ mainjs/main.js
@@ -0,0 +1,23 @@
+
+console.log("main loaded");
+
+let myNumber;
+
+
+
+
+
+
+document.querySelectorClass = ("something").innerHTML = 'je hebt verloren';
+
+
+function getRandomNumber (max) {
+    return Math.floor(Math.random() * max) + 2;
+   
+}
+
+function rollDice() {
+    myNumber = getRandomNumber(11);
+    console.log('random getal is ' + myNumber);
+}
+document.querySelectorClass = ("something").innerHTML = 'je hebt verloren';
