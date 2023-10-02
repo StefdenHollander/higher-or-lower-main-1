72ffd4275168e4ecc9f5a991eeba70de55fd9ecb
S den Hollander <302045198@student.rocmondriaan.nl>
2023-10-02 12:58:54 +0200

    update

--- mainjs/main.js
+++ mainjs/main.js
@@ -1,18 +1,20 @@
 
 console.log("main loaded");
 
-let myNumber;
+let myNumber = "";
+let gameStarted = false;
 
-
-
-
+let myButton = document.querySelector(".myButton");
 
 
 document.querySelectorClass = ("something").innerHTML = 'je hebt verloren';
 
+function showMessage(message) {
+    document.querySelector(".interface").textContent = message;
+}
 
-function getRandomNumber (max) {
-    return Math.floor(Math.random() * max) + 2;
+function getRandomNumber () {
+    return Math.floor(Math.random() * 6) + 1;
    
 }
 
@@ -21,5 +23,27 @@ function rollDice() {
     console.log('random getal is ' + myNumber);
 }
 
-Alert(getRandomNumber)
+myButton = document.addEventListener("click", function(){
+    rollDice();
+})
+
+
+document.querySelector(".higher-button").addEventListener("click", () => {
+    setupGame("higher-button");
+});
+
+document.querySelector(".higher-button").addEventListener("click", () => {
+    setupGame("lower-button");
+})
+
+document.querySelector(".higher-button").addEventListener("click", () => {
+    setupGame("myButton");
+})
+
+function setupGame(guess) {
+    if (!gameStarted){
+        gameStarted = true;
+
+    }
+}
 //document.querySelectorClass = ("something").innerHTML = 'je hebt verloren';
